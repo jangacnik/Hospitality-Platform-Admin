e542aa5816c76cf6d2edea94c491a595ccfd8f94
Jan Gacnik <nqmrnr7xgg@privaterelay.appleid.com>
2023-09-19 15:10:17 +0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './user/login/login.component';
+import { ProfileComponent } from './user/profile/profile.component';
+import { DashboardComponent } from './foodtracker/dashboard/dashboard.component';
+import { ReportComponent } from './foodtracker/report/report.component';
+import { UserManagmentComponent } from './foodtracker/user-managment/user-managment.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ProfileComponent,
+    DashboardComponent,
+    ReportComponent,
+    UserManagmentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
